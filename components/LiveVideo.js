961b84c02eb04490b2a8dfd5ecc72a0e10b5c4cc
Adebayo Wisdom <adebayowisdom796@gmail.com>
2023-10-27 17:53:03 +0100

    new commit update on youtube data api

--- components/LiveVideo.js
+++ components/LiveVideo.js
@@ -1,26 +1,22 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import Loader from './Loader';
 
 function LiveVideo() {
-  const [videoData, setVideoData] = useState([]);
+  const [videoData, setVideoData] = useState(null);
 
   const fetchChannelVideos = async (channelId) => {
     const part = 'snippet';
     const maxResults = 1;
     const type = 'video';
+    const eventType = 'live'; // Filter for live videos
     const apiKey = 'AIzaSyB5kDGlj_ez1SQBHUO0lSusWZalKAEJeRw'; // Replace with your actual API key
-    const eventType = 'live'
-    const apiUrl = `https://www.googleapis.com/youtube/v3/search?part=${part}&maxResults=${maxResults}&type=${type}&eventType=${eventType}&channelId=${channelId}&key=${apiKey}`;
 
+    const apiUrl = `https://www.googleapis.com/youtube/v3/search?part=${part}&maxResults=${maxResults}&type=${type}&eventType=${eventType}&channelId=${channelId}&key=${apiKey}`;
 
     try {
       const response = await axios.get(apiUrl);
       if (response.data.items.length > 0) {
         setVideoData(response.data.items[0]);
-      } else {
-        // No live videos found
-        setVideoData(null);
       }
     } catch (error) {
       console.error('Error fetching videos:', error);
@@ -28,25 +24,15 @@ function LiveVideo() {
   };
 
   useEffect(() => {
-    const fetchChannelLiveVideos = async () => {
-      try {
-        const channelId = 'UCT2tbCh0APZOTgBh2eMSHeg'; // Replace with the actual channel ID
-        const videos = await fetchChannelVideos(channelId);
-        setVideoData(videos);
-      } catch (error) {
-        console.error('Error fetching videos:', error);
-      }
-    };
-
-    fetchChannelLiveVideos();
+    const channelId = 'UCT2tbCh0APZOTgBh2eMSHeg'; // Replace with the actual channel ID
+    fetchChannelVideos(channelId);
   }, []);
 
-  if (videoData && videoData.length === 0) {
+  if (!videoData) {
     return <p>Loading...</p>;
   }
 
-
-  const videoId = videoData && videoData.id.videoId;
+  const videoId = videoData.id.videoId;
   const videoUrl = `https://www.youtube.com/embed/${videoId}`;
 
   return (
